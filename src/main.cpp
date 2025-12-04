d8c83bfb8de071815729aa1c86bae0767c99e82f
miguelmagalhaes17 <miguelmagalhaes98@gmail.com>
2025-12-04 18:55:26 +0000

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,38 @@
 #include <Wire.h>
 #include <Adafruit_LSM303.h>
 
+// -----------------------------------------------------------------------------------------------------
+
 // Accelerometer objects
 Adafruit_LSM303 accel1;
 Adafruit_LSM303 accel2;
 
+// -----------------------------------------------------------------------------------------------------
+
 // Pin definitions
+// Accelerometer Pins
 #define ACCEL1_SDA_PIN 12
 #define ACCEL1_SCL_PIN 13
 #define ACCEL2_SDA_PIN 14
 #define ACCEL2_SCL_PIN 15
 
-#define B1BUTTON_PIN 16
-#define B2BUTTON_PIN 17
+// Button Pins
+#define SELBUTTON_PIN 16
+#define CYCLEBUTTON_PIN 17
 
+// LCD Pins
 #define LCD1_CLK_PIN 11
 #define LCD1_DIN_PIN 10
 #define LCD1_DC_PIN 9
 #define LCD1_CS_PIN 8
 #define LCD1_RST_PIN 7
-
 #define LCD2_CLK_PIN 6
 #define LCD2_DIN_PIN 5
 #define LCD2_DC_PIN 4
 #define LCD2_CS_PIN 3
 #define LCD2_RST_PIN 2
 
+// -----------------------------------------------------------------------------------------------------
 
 // RMS Task Structure
 typedef struct RmsTask {
@@ -38,12 +45,6 @@ typedef struct RmsTask {
   TaskHandle_t handle;
 } RmsTask;
 
-// Task function declarations
-void TaskFast(void* pv);
-void TaskMedium(void* pv);
-void TaskSlow(void* pv);
-//void TaskAccel(void* pv);  // New accelerometer task
-
 // Task array
 RmsTask tasks[] = {
   { TaskFast,   "FAST",    4096, 10,   0, NULL },
@@ -53,6 +54,19 @@ RmsTask tasks[] = {
 };
 
 int taskCount = sizeof(tasks) / sizeof(tasks[0]);
+int selectButtonState = LOW;
+int cycleButtonState = LOW;
+
+// -----------------------------------------------------------------------------------------------------
+
+// Task function declarations
+void TaskFast(void* pv);
+void TaskMedium(void* pv);
+void TaskSlow(void* pv);
+void TaskReadButtons(void* pv);
+//void TaskAccel(void* pv);  // New accelerometer task
+
+// -----------------------------------------------------------------------------------------------------
 
 // RMS Priority Assignment
 void assignRmsPriorities() {
@@ -115,6 +129,29 @@ void TaskSlow(void* pv) {
     vTaskDelay(pdMS_TO_TICKS(tasks[2].periodMs));
   }
 }
+
+/*void TaskReadButtons()
+{
+  selectButtonState = digitalRead(SELBUTTON_PIN);
+  cycleButtonState = digitalRead(CYCLEBUTTON_PIN);
+}
+void TaskReadAccel1()
+{
+
+}
+void TaskReadAccel2()
+{
+
+}
+void TaskDisplayLCD()
+{
+
+}
+void TaskGameLogic()
+{
+
+}*/
+
 /*
 void TaskAccel(void* pv) {
   // Initialize accelerometers
@@ -159,6 +196,27 @@ void TaskAccel(void* pv) {
 void setup()
 {
   Serial.begin(115200);
+  
+  // I2C Accelerometer init
+  Wire.begin(ACCEL1_SDA_PIN, ACCEL1_SCL_PIN);
+  Wire1.begin(ACCEL2_SDA_PIN, ACCEL2_SCL_PIN);
+
+  if(!accel1.begin()) {
+    Serial.println("Accelerometer 1 not found!");
+  } else {
+    Serial.println("Accelerometer 1 initialized");
+  }
+  
+  if(!accel2.begin()) {  // Use Wire1 for second accelerometer
+    Serial.println("Accelerometer 2 not found!");
+  } else {
+    Serial.println("Accelerometer 2 initialized");
+  }
+
+  // Button init
+  pinMode(SELBUTTON_PIN, INPUT_PULLUP);
+  pinMode(CYCLEBUTTON_PIN, INPUT_PULLUP);
+  
   delay(1000);  // Give serial time to initialize
   
   Serial.println("\n=== RMS Scheduler with Accelerometers ===");
@@ -175,5 +233,4 @@ void loop()
   // Empty - all work is done in FreeRTOS tasks
   // This task runs at lowest priority (1 by default)
   vTaskDelay(portMAX_DELAY);  // Sleep forever
-
 }
