3c5199ab42077e07bec97a136bad1bf95db4fa8c
André Osório <up202004732@up.pt>
2025-12-18 18:31:10 +0000

    Task Timing WIP

--- lib/Utils/Utils.hpp
+++ lib/Utils/Utils.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
-#define DEBUG // Uncomment for debug messages	
+#include <Arduino.h>
+
+#define DEBUG // Uncomment for debug messages
+#define TIME_TASKS // Uncomment to get time information about tasks
+
+// Task Timing Structure
+typedef struct TaskTiming {
+  uint64_t timeSum;
+  uint64_t timeCount;
+  int timeMax;
+  int timeMin;
+} TaskTiming;
